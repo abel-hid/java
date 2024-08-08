075112615954b1790b9232c11dab12ea38a98764
Abdeljalil El Hidaoui <abdeljalilelhidaoui21@gmail.com>
2024-08-08 09:24:49 +0100

    add multiple Reviews for a Product

--- labs/parctices/ProductManagement/src/labs/pm/app/Shop.java
+++ labs/parctices/ProductManagement/src/labs/pm/app/Shop.java
@@ -19,28 +19,13 @@ public class Shop {
         Product p1 = pm.createProduct(101, "Tea", BigDecimal.valueOf(1.99), NOT_RATED);
        
         pm.printProductReport();
-        p1 = pm.reviewProduct(p1, Rating.FOUR_STAR, "Nice hot cup of tea");
+        p1 = pm.reviewProduct(p1, Rating.THREE_STAR, "Nice hot cup of tea");
+        p1 = pm.reviewProduct(p1, Rating.TWO_STAR, "Rather weak tea");
+        p1 = pm.reviewProduct(p1, Rating.FOUR_STAR, "Fine tea");
+        p1 = pm.reviewProduct(p1, Rating.FOUR_STAR, "Good tea");
+        p1 = pm.reviewProduct(p1, Rating.FIVE_STAR, "Perfect tea");
+        p1 = pm.reviewProduct(p1, Rating.THREE_STAR, "Just add some lemon");
         pm.printProductReport();
-    //     Product p2 = pm.createProduct(102, "Coffee", BigDecimal.valueOf(1.99), FOUR_STAR);
-    //     Product p3 = pm.createProduct(103, "Cake", BigDecimal.valueOf(3.99), FIVE_STAR , LocalDate.now().plusDays(2));
-    //     Product p4 = pm.createProduct(105, "Cookie", BigDecimal.valueOf(2.99), TWO_STAR , LocalDate.now().plusDays(2));
-
-    //     Product p5 = p3.applyRating(THREE_STAR);
-    //     // System.out.println(p5.getId() + " " + p5.getName() + " " + p5.getPrice() + " " + p5.getDiscount() + " " + p5.getRating().getStars());
-    //     Product p6 =  pm.createProduct(104, "Chocolate", BigDecimal.valueOf(2.99), FIVE_STAR);
-    //     Product p7 =  pm.createProduct(104, "Chocolate", BigDecimal.valueOf(2.99), FIVE_STAR , LocalDate.now().plusDays(2));
-    //     Product p8 =  p4.applyRating(FIVE_STAR);
-    //     Product p9 =  p1.applyRating(TWO_STAR);
-    //     System.err.println(p1);
-    //     System.err.println(p2);
-    //     System.err.println(p3);
-    //     System.err.println(p4);
-    //     System.err.println(p5);
-    //     System.err.println(p6.equals(p7));
-    //     System.err.println(p8);
-    //     System.err.println(p9);
-    //    System.out.println(p3.getBestBefore());
-    //    System.out.println(p1.getBestBefore());
         
     }
 }
--- labs/parctices/ProductManagement/src/labs/pm/data/ProductManager.java
+++ labs/parctices/ProductManagement/src/labs/pm/data/ProductManager.java
@@ -1,4 +1,5 @@
 package labs.pm.data;
+import java.lang.reflect.Array;
 import java.math.BigDecimal;
 import java.text.MessageFormat;
 import java.text.NumberFormat;
@@ -11,11 +12,13 @@ import java.util.ResourceBundle;
 import labs.pm.data.Product;
 import labs.pm.data.Review;
 import labs.pm.data.Rating;
+import java.util.Arrays;
 
 public class ProductManager {
     private Product product;
 
-    private Review review;
+    // private Review review;
+    private Review[] reviews = new Review[5];
     
     private ResourceBundle resources;
     private Locale locale;
@@ -47,14 +50,35 @@ public class ProductManager {
 
     public Product reviewProduct(Product product, Rating rating, String comments) 
     {
-        review = new Review(rating, comments);
-        this.product = product.applyRating(rating);
+        // review = new Review(rating, comments);
+        if(reviews[reviews.length - 1] != null)
+        {
+            // Review[] temp = new Review[reviews.length + 5];
+            // System.arraycopy(reviews, 0, temp, 0, reviews.length);
+            // reviews = temp;
+            reviews = Arrays.copyOf(reviews, reviews.length + 5);
+        }
+        int sum = 0;
+        boolean reviewed = false;
+        int i = 0;
+        while(!reviewed && i < reviews.length)
+        {
+            if(reviews[i] == null)
+            {
+                reviews[i] = new Review(rating, comments);
+                reviewed = true;
+            }
+            sum += reviews[i].rating().ordinal();
+            i++;
+        }
+        this.product = product.applyRating(Rateable.convert(Math.round((float)sum / i)));
         return this.product;
 
     }
 
     public void printProductReport() {
-        if (product == null) {
+        if (product == null)
+        {
             System.out.println("No product available.");
             return;
         }
@@ -79,13 +103,23 @@ public class ProductManager {
                 type));
     
         txt.append("\n");
-        if (review != null) {
+
+        for (Review review : reviews)
+        {
+            if (review == null) {
+                break;
+            }
             txt.append(MessageFormat.format(resources.getString("review"),
                     review.rating().getStars(), review.comments()));
-        } else {
-            txt.append(resources.getString("no.review"));
+            txt.append("\n");
+
         }
-        txt.append("\n");
+
+        if(reviews[0] == null)
+        {
+            txt.append(resources.getString("no.review"));
+            txt.append("\n");
+        }
         System.out.println(txt);
     }
     
