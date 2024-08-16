e1afca04a000b255646d88ec2ed7aea228a2913c
guts <116905215+abel-hid@users.noreply.github.com>
2024-08-16 01:26:11 +0100

    add swap mechanism

--- labs/parctices/ProductManagement/src/labs/pm/app/Shop.java
+++ labs/parctices/ProductManagement/src/labs/pm/app/Shop.java
@@ -13,7 +13,18 @@ public class Shop {
 
         ProductManager pm = new ProductManager("en-GB");
          pm.printProductReport(101);
-         pm.printProductReport(10);
+         pm.createProduct(101, "Tea", BigDecimal.valueOf(1.99), NOT_RATED);
+            pm.reviewProduct(101, FOUR_STAR, "Nice hot cup of tea");
+            pm.reviewProduct(101, TWO_STAR, "Rather weak tea");
+            pm.reviewProduct(101, FOUR_STAR, "Fine tea");
+            pm.reviewProduct(101, THREE_STAR, "Good tea");
+            pm.reviewProduct(101, FIVE_STAR, "Perfect tea");
+            pm.reviewProduct(101, ONE_STAR, "Horrible tea");
+            pm.dumpData();
+            pm.restoreData();
+            pm.printProductReport(101);
+
+            pm.printProducts(p -> p.getPrice().floatValue() < 2, (p1, p2) -> p2.getRating().ordinal() - p1.getRating().ordinal());
     }
 
 }
